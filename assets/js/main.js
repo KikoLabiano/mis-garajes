1b89a0c0596d75d5a38bf95573bafd2e8ce172ea
Kiko <fjlabiano@itracasa.es>
2022-01-02 15:39:47 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,82 @@
 (function () {
   "use strict";
 
+  function magnify(imgID, zoom) {
+    var img, glass, w, h, bw;
+    img = document.getElementById(imgID);
+    /*create magnifier glass:*/
+    glass = document.createElement("DIV");
+    glass.setAttribute("class", "img-magnifier-glass");
+    /*insert magnifier glass:*/
+    img.parentElement.insertBefore(glass, img);
+    /*set background properties for the magnifier glass:*/
+    glass.style.backgroundImage = "url('" + img.src + "')";
+    glass.style.backgroundRepeat = "no-repeat";
+    glass.style.backgroundSize =
+      img.width * zoom + "px " + img.height * zoom + "px";
+    bw = 3;
+    w = glass.offsetWidth / 2;
+    h = glass.offsetHeight / 2;
+    /*execute a function when someone moves the magnifier glass over the image:*/
+    glass.addEventListener("mousemove", moveMagnifier);
+    img.addEventListener("mousemove", moveMagnifier);
+    /*and also for touch screens:*/
+    glass.addEventListener("touchmove", moveMagnifier);
+    img.addEventListener("touchmove", moveMagnifier);
+
+    function moveMagnifier(e) {
+      var pos, x, y;
+      /*prevent any other actions that may occur when moving over the image*/
+      e.preventDefault();
+      /*get the cursor's x and y positions:*/
+      pos = getCursorPos(e);
+      x = pos.x;
+      y = pos.y;
+      /*prevent the magnifier glass from being positioned outside the image:*/
+      if (x > img.width - w / zoom) {
+        x = img.width - w / zoom;
+      }
+      if (x < w / zoom) {
+        x = w / zoom;
+      }
+      if (y > img.height - h / zoom) {
+        y = img.height - h / zoom;
+      }
+      if (y < h / zoom) {
+        y = h / zoom;
+      }
+      /*set the position of the magnifier glass:*/
+      glass.style.left = x - w + "px";
+      glass.style.top = y - h + "px";
+      /*display what the magnifier glass "sees":*/
+      glass.style.backgroundPosition =
+        "-" + (x * zoom - w + bw) + "px -" + (y * zoom - h + bw) + "px";
+    }
+
+    function getCursorPos(e) {
+      var a,
+        x = 0,
+        y = 0;
+      e = e || window.event;
+      a = img.getBoundingClientRect();
+      x = e.pageX - a.left;
+      y = e.pageY - a.top;
+
+      x = x - window.pageXOffset;
+      y = y - window.pageYOffset;
+      return { x: x, y: y };
+    }
+  }
+
   // ======= Sticky
   window.onscroll = function () {
     const ud_header = document.querySelector(".ud-header");
     const sticky = ud_header.offsetTop;
-    const logo = document.querySelector(".navbar-brand img");
 
     if (window.pageYOffset > sticky) {
       ud_header.classList.add("sticky");
     } else {
       ud_header.classList.remove("sticky");
-    }
-
-    // === logo change
-    if (ud_header.classList.contains("sticky")) {
-      logo.src = "assets/images/logo/logo-2.svg";
-    } else {
-      logo.src = "assets/images/logo/logo.svg";
     }
 
     // show or hide the back-top-top button
@@ -124,5 +183,16 @@
         console.log(plazaInfo.style.left, plazaInfo.style.top);
       });
     });
+
+    if (!navigator.userAgentData.mobile) {
+      magnify("lezkairu1", 3);
+      magnify("lezkairu2", 3);
+      magnify("bardenasReales1", 3);
+      magnify("bardenasReales2", 3);
+      magnify("castilloIrulegui", 3);
+      magnify("palacioGongora1", 3);
+      magnify("palacioGongora2", 3);
+      magnify("valleAranguren", 3);
+    }
   });
 })();
